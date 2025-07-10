3c4bf9bf8bba81299c9c839f26d4c4486149b1ce
kapindo <kapindo@gmail.com>
2025-07-10 10:27:14 +0700

    create operate function and variable variable to store number and operator sign function to calculate based operator given

--- app.js
+++ app.js
@@ -14,3 +14,19 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
+let number1;
+let number2;
+let operator;
+
+function operate(opr, num1, num2) {
+  switch (opr) {
+    case "÷":
+      divide(num1, num2);
+    case "*":
+      multiply(num1, num2);
+    case "-":
+      subtract(num1, num2);
+    case "+":
+      add(num1, num2);
+  }
+}
